1875b6203c3e3cb7e599d98a7603a55ca3b5c100
galibremo <galibremo@gmail.com>
2024-04-09 00:32:54 +0600

    Add next auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Image from "next/image";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 import Link from "next/link";
 import React from "react";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex items-center justify-between max-w-7xl mx-auto">
@@ -12,6 +19,8 @@ export default function Header() {
             width={96}
             height={96}
             alt="instagram logo"
+            priority
+            styles={{ width: "auto" }}
           />
         </Link>
         <Link href="/" className="lg:hidden">
@@ -27,7 +36,23 @@ export default function Header() {
           placeholder="Search"
           className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <div className="flex gap-2 items-center">
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
+          </div>
+        ) : (
+          <button
+            onClick={signIn}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
