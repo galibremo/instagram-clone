4129dfc936deba28b8a9ba0ae4fa3a9fface90e6
galibremo <galibremo@gmail.com>
2024-04-09 01:11:54 +0600

    Add modal functionality using react modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,19 @@ import { IoMdAddCircleOutline } from "react-icons/io";
 import { HiCamera } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import Link from "next/link";
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
+import Modal from "react-modal";
 
 export default function Header() {
+  const filePickerRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
   const { data: session } = useSession();
+
+  function addImageToPost() {}
+  function handleSubmit() {}
+
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex items-center justify-between max-w-7xl mx-auto">
@@ -38,6 +46,10 @@ export default function Header() {
         />
         {session ? (
           <div className="flex gap-2 items-center">
+            <IoMdAddCircleOutline
+              className="text-2xl cursor-pointer tranform hover:scale-125 transition duration-300 hover:text-red-600"
+              onClick={() => setIsOpen(true)}
+            />
             <img
               src={session.user.image}
               alt={session.user.name}
@@ -54,6 +66,42 @@ export default function Header() {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-64 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <HiCamera className="text-5xl text-gray-400 cursor-pointer" />
+            <input
+              hidden
+              ref={filePickerRef}
+              type="file"
+              accept="image/*"
+              onChange={addImageToPost}
+            />
+          </div>
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter you caption..."
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+          />
+          <button
+            onClick={handleSubmit}
+            disabled
+            className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
