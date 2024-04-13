9317ef3d26f0ca6e3308ffbe13084022d2cbd23a
galibremo <galibremo@gmail.com>
2024-04-14 00:16:14 +0600

    Make Feed Section Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Feed from "@/components/Feed";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Feed />
+    </>
+  );
 }
