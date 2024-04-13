c43297e2ae424a12db40b3097e553a4aedfec05c
galibremo <galibremo@gmail.com>
2024-04-14 00:26:05 +0600

    Complete MiniProfile Page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,7 +9,9 @@ export default function Feed() {
         <Post />
       </section>
       <section className="hidden md:inline-grid md:col-span-1">
-        <MiniProfile />
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+        </div>
       </section>
     </main>
   );
