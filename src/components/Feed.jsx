eb5cd77f623fe0be1067eed91096bc52877a8a25
galibremo <galibremo@gmail.com>
2024-04-14 01:00:44 +0600

    Complete The Post Card of The Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Post from "./Post";
 import MiniProfile from "./MiniProfile";
+import Posts from "./Posts";
 
 export default function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-7xl mx-auto">
       <section className="md:col-span-2">
-        <Post />
+        <Posts />
       </section>
       <section className="hidden md:inline-grid md:col-span-1">
         <div className="fixed w-[380px]">
